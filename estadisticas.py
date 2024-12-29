06f9f3b7de5b74e203e0d54ea0a32599c84d6177
Gary Roco <rocohachi@gmail.com>
2024-12-28 21:18:07 -0300

    estadisticas.py

---
+++ estadisticas.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def pan_mas_y_menos_vendido():
+   
+
+def franja_horaria_mas_y_menos_ventas():
+
+def ganancia_e_inversion_total():
+
+def ganancia_liquida_meta(): #?
+
+def grafico_barras_ganancia():
+
+def grafico_circular_ganancia_vs_costo():

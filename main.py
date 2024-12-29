45f975ff511b74a38aac2e3128cfd06a0f84b0fa
Gary Roco <rocohachi@gmail.com>
2024-12-29 00:42:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 import recetas
 import estadisticas
 import time as t
+#LISTAS CON NOMBRES
 recursos_nombre: ["Harina","Agua","Levadura","Manteca","Azucar","Sal"]
-recursos = [0,0,0,0,0,0]
-recursos_repartidos = [0,0,0,0,0,0]
 pan_nombre = ["Hallulla","Marraqueta","Dobladitas","Ciabatta"
               ,"Pan de completo","Coliza","Integral","Pan de hamburguesa"]
+#RECURSOS
+recursos = [0,0,0,0,0,0]
+recursos_repartidos = [0,0,0,0,0,0]
+#PRODUCCION DIARIA
 produccion_diaria = [0,0,0,0,0,0,0,0]
-#matriz
+#MATRIZ DE VENTAS
 ventas_diarias = [
   [0,0,0]
   [0,0,0]
@@ -20,65 +23,86 @@ ventas_diarias = [
   [0,0,0]
   [0,0,0]
 ]
+#PRECIOS
+precios_pan = [2190,2290,3190,3290,2990,3390,2850,2990]
+precios_recursos = [50,1000,6000,2500,1500,400]
+#META
 meta_ganancia = 0
+#Contador dias
+contador_dia = 0
+
 #funcion para registro diario
 def inicio_registro():
-  global recursos,produccion_diaria,meta_ganancia
-  print("------Registro de recursos diarios------"")
-  recursos[0] = float(input("Ingrese la cantidad de harina en kg: "))
-  recursos[1] = float(input("Ingrese la cantidad de agua en kg: "))
-  recursos[2] = float(input("Ingrese la cantidad de levadura en kg: "))
-  recursos[3] = float(input("Ingrese la cantidad de manteca en kg: "))
-  recursos[4] = float(input("Ingrese la cantidad de azucar en kg: "))
-  recursos[5] = float(input("Ingrese la cantidad de sal en kg: "))
-  meta_ganancia = float(input("Ingrese la meta de ganancia liquida: "))
-  t.sleep(1)
-  print("Calculando produccion_diaria")
-  recursos_repartidos = recursos_repartidos(recursos)
-  produccion_diaria = recetas.calcular_produccion_diaria(recursos_repartidos)
-  t.sleep(1)
-  print("Producción calculada con exito...")
-  t.slepp(1)
-  recetas.mostrar_produccion(produccion_diaria,pan_nombre)
+    global recursos,produccion_diaria,meta_ganancia
+    print("------Registro de recursos diarios------")
+    recursos[0] = float(input("Ingrese la cantidad de harina en kg: "))
+    recursos[1] = float(input("Ingrese la cantidad de agua en kg: "))
+    recursos[2] = float(input("Ingrese la cantidad de levadura en kg: "))
+    recursos[3] = float(input("Ingrese la cantidad de manteca en kg: "))
+    recursos[4] = float(input("Ingrese la cantidad de azucar en kg: "))
+    recursos[5] = float(input("Ingrese la cantidad de sal en kg: "))
+    meta_ganancia = float(input("Ingrese la meta de ganancia liquida: "))
+    t.sleep(1)
+    print("Calculando produccion_diaria")
+    recursos_repartidos = recursos_repartidos(recursos)
+    produccion_diaria = recetas.calcular_produccion_diaria(recursos_repartidos)
+    t.sleep(1)
+    print("Producción calculada con exito...")
+    t.slepp(1)
+    recetas.mostrar_produccion(produccion_diaria,pan_nombre)
 
-  menu_dia()
+    menu_dia()
 
 #funcion menu control del dia
 def menu_dia():
-  while True:
-    print("-------Menu del dia-------"")
-    print("1. Registrar ventas"")
-    print("2. Visualizar ventas registradas")
-    print("3. Generar estadisticas")
-    print("4. Finalizar dia y generar informe")
-    print("5. Volver al menu principal"")
-    o = input("Escoja una opcion (1-5):")
-    if o == "1":
-    
-    elif o == "2":
+    global contador_dia
+    while True:
+        print("-------Menu del dia-------")
+        print("1. Registrar ventas")
+        print("2. Visualizar ventas registradas")
+        print("3. Generar estadisticas")
+        print("4. Finalizar dia y generar informe")
+        print("5. Volver al menu principal")
+        o = input("Escoja una opcion (1-5):")
+        if o == "1":
+            recetas.registrar_ventas(produccion_diaria,ventas_diarias,pan_nombre)
+        
+        elif o == "2":
+            recetas.mostrar_ventas(ventas_diarias,pan_nombre)
 
-    elif o == "3":
+        elif o == "3":
+            estadisticas.menu_estadisticas(produccion_diaria, ventas_diarias, meta_ganancia,precios_pan,precios_recursos)
 
-    elif o == "4":
+        elif o == "4":
+            o2 = input("Esta seguro de terminar el dia y generar el informe? (si/no): ")
+            if o2.lower() == "si":
+                contador_dia += 1
+                estadisticas.finalizar_dia(ventas_diarias, produccion_diaria, meta_ganancia, contador_dia)
+                break
+            elif o2.lower() == "no":
+                pass
+            else:
+                print("Opcion no valida.")
+                pass
 
-    elif o == "5":
-      break
-    else:
-      print("Opcion invalida, Intente de nuevo. ")
+        elif o == "5":
+            break
+        else:
+            print("Opcion invalida, Intente de nuevo. ")
 
 #Menu principal
 while True:
-  print("----------Sistema de Gestion de amasanderia----------"")
-  print("1. Iniciar registro diario. ")
-  print("2. Salir del sistema. ")
-  o = input("Escoja una opcion (1/2): ")
-  if o == "1":
-    print("Iniciando registro diario. ")
-    incio_registro()
-  
-  elif o == "2":
-    print("Saliendo del sistema. ")
-    break
-  else:
-    print("Porfavor Ingrese una opcion valida. ")
+    print("----------Sistema de Gestion de amasanderia----------")
+    print("1. Iniciar registro diario. ")
+    print("2. Salir del sistema. ")
+    o = input("Escoja una opcion (1/2): ")
+    if o == "1":
+        print("Iniciando registro diario. ")
+        recetas.incio_registro()
+    
+    elif o == "2":
+        print("Saliendo del sistema. ")
+        break
+    else:
+        print("Porfavor Ingrese una opcion valida. ")
     
